17c1e4432b14cdc158eedff3e1a782cfd10ff2b6
Halima <152661929+Halima-Ablat@users.noreply.github.com>
2025-01-09 21:54:25 +1100

    Update TextPopUp.jsx

--- TextPopUp.jsx
+++ TextPopUp.jsx
@@ -6,7 +6,7 @@ function TextPopUp() {
 
   function handleClick(){
     textFocus.current.focus();
-    textFocus.current.value = "Hello, my name is Deny Dog 🐶."
+    textFocus.current.value = "Hello, my name is Deny Dog 🐶, nice to meet you!"
   }
 
   return (
